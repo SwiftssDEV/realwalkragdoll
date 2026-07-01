96b1cbd009c80514e1cbba0b9118da7c296c88a4
SwiftssDEV <kumikumihabahabalakalaka@mail.ru>
2026-07-01 16:14:27 +0500

    Delete part4.js

--- part4.js
+++
@@ -1,7 +0,0 @@
-// Код сборки финальной закладки
-const fullBookmarkletCode = "(function(){" + window.p1 + window.p2 + "window.updateGame();})();";
-const dragBtn = document.getElementById("drag-btn");
-if(dragBtn){
-    dragBtn.href = "javascript:" + encodeURIComponent(fullBookmarkletCode);
-    dragBtn.innerText = "🚀 КИБЕР-ОТРЯД: СТИКМЕН В ЗАКЛАДКУ";
-}
